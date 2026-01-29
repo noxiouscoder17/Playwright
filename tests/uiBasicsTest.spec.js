d57bd830c552132c2455cb5fd07376220323e949
Bhavishya Pawar <pawarbhavishya17@gmail.com>
2026-01-30 03:36:51 +0530

    Annotations, Async, Anonymous Functions, fixtures - browser, page

---
+++ tests/uiBasicsTest.spec.js
@@ -0,0 +1,12 @@
+const {test} = require('@playwright/test');
+
+test('Browser Playwright Test', async ({browser})=>{
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+});
+
+test('Page Playwright Test', async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+});
